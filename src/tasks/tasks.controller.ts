ea6ef2a6351ac899c6c18a93ba9d4307d863b893
wijaksanapanji <wijaksanapanji6@gmail.com>
2021-08-15 19:27:40 +0800

    add logging example

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import {
     Controller,
     Delete,
     Get,
+    Logger,
     Param,
     Patch,
     Post,
@@ -21,6 +22,8 @@ import { TasksService } from './tasks.service';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+    private logger = new Logger('TasksController');
+
     constructor(private readonly tasksService: TasksService) {}
 
     @Get()
@@ -28,6 +31,7 @@ export class TasksController {
         @Query() filterDto: GetTasksFilterDto,
         @GetUser() user: User,
     ): Promise<Task[]> {
+        this.logger.verbose(`User ${user.username} retrieving all tasks`);
         return this.tasksService.getTasks(filterDto, user);
     }
 
